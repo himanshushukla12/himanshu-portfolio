c9d35dd116c7d6422ddec45f4857e221da6ca015
Himanshu Shukla <himanshushukla.shukla3@gmail.com>
2023-12-31 23:02:09 +0530

    Updates

---
+++ precache-manifest.a2c537a55af6a85b240e608ace681f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8654656e36c648c48a8ad0d1eb678848",
+    "url": "/himanshu-portfolio/index.html"
+  },
+  {
+    "revision": "e57d233cd599c562c7de",
+    "url": "/himanshu-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "efb6ef606a16353b7351",
+    "url": "/himanshu-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "e57d233cd599c562c7de",
+    "url": "/himanshu-portfolio/static/js/2.177ae940.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/himanshu-portfolio/static/js/2.177ae940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb6ef606a16353b7351",
+    "url": "/himanshu-portfolio/static/js/main.27c516c5.chunk.js"
+  },
+  {
+    "revision": "3d4c27fea0921184d255",
+    "url": "/himanshu-portfolio/static/js/runtime-main.3c140137.js"
+  }
+]);
